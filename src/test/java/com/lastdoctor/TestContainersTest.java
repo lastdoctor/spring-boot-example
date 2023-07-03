9d2f04230d1682a11d37686a4e6317e241f13382
Susa Kubatov <melastdoctorwho@gmail.com>
2023-07-04 02:45:29 +0800

    add abstract test

--- src/test/java/com/lastdoctor/TestContainersTest.java
+++ src/test/java/com/lastdoctor/TestContainersTest.java
@@ -1,21 +1,12 @@
 package com.lastdoctor;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class TestContainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.3-alpine")
-            .withDatabaseName(" ")
-            .withUsername("postgres")
-            .withPassword("postgres");
-
+public class TestContainersTest extends AbstractTestContainersUnitTest {
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
